a366dcb02ef28f435685ec263f6065dbb5f22590
m.naderkhani <m.naderkhani@example.com>
2026-07-06 15:02:55 +0330

    chore: delete everything to rewrite project

--- apps/game/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite-plus";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { lazyPlugins } from "vite-plus";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: lazyPlugins(() => [svelte()]),
-});
